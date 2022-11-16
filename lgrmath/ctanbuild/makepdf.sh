30c9bfe2b24c70becea1dd7d97ad4c7ab231fe3f
jfbu <2589111+jfbu@users.noreply.github.com>
2022-11-16 21:52:09 +0100

    Update makepdf.sh to use xattr -c if available

--- lgrmath/ctanbuild/makepdf.sh
+++ lgrmath/ctanbuild/makepdf.sh
@@ -5,6 +5,7 @@ echo "Building pdf in ctanbuild/ ..."
 cd $dir
 etex lgrmath.dtx
 latexmk -e "\$dvipdf = q/dvipdfmx %O -o %D %S/" -pdfdvi lgrmath.tex
+command -v xattr 2> /dev/null && xattr -c lgrmath.pdf
 cd ..
 echo "... done"
 
